8414e39c6ba0d6013fbadcaf82003d7562106850
Nazanin Hatami <nazaninhatami.leet@gmail.com>
2025-07-04 14:35:07 +0330

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from './components/navbar';
+import Hero from './components/hero';
+import About from './components/about';
+import Services from './components/servicesSection';
+import Tours from './components/tours';
+import Footer from './components/footer';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
